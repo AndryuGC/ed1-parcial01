ddafe42844b46dfafa906da176ad5db92f9dc6a4
AndryuGC <andryupgc@gmail.com>
2025-03-25 07:47:53 -0600

    Parcial 01 - Estructura de Datos I

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -1,9 +1,25 @@
 package ed.lab;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class E01FlattenBT {
 
     public void flatten(TreeNode<Integer> root) {
+        if (root == null) return;
 
+        TreeNode Lista = root;
+        while (Lista != null) {
+            if (Lista.left != null) {
+                TreeNode Nodo = Lista.left;
+                while (Nodo.right != null) {
+                    Nodo = Nodo.right;
+                }
+                Nodo.right = Lista.right;
+                Lista.right = Lista.left;
+                Lista.left = null;
+            }
+            Lista = Lista.right;
+        }
     }
-
-}
+    }
